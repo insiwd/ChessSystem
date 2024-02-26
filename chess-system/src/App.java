4461bbb15e4f1a3cf86165e23f8049b4e1a43546
ruan <ruan.cavalcante505@gmail.com>
2024-02-26 14:56:20 -0300

    feat: new class position

--- chess-system/src/App.java
+++ chess-system/src/App.java
@@ -1,5 +1,9 @@
+import boardgame.Position;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
